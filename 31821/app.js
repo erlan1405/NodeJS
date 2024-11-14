d9cdd4a2fce512cabd4a37f5f7f8f5cc0ae75301
erlan1405 <erlander5551@gmail.com>
2024-11-14 17:22:38 -0500

    create port variable

--- 31821/app.js
+++ 31821/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Example EJS', message: 'This is EJS.', isLoggedIn: true });
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
